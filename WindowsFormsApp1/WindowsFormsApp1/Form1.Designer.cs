7afec063b204b0f565563151b4d2419ee2cc9201
Anastasia Abazovskaya <ryabayeah@gmail.com>
2021-04-22 12:33:44 +0700

    create

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.question1 = new System.Windows.Forms.TextBox();
+            this.answer1 = new System.Windows.Forms.Button();
+            this.random1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericYes = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericYes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.numericYes);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.random1);
+            this.panel1.Controls.Add(this.question1);
+            this.panel1.Controls.Add(this.answer1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(451, 325);
+            this.panel1.TabIndex = 0;
+            // 
+            // question1
+            // 
+            this.question1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.question1.Location = new System.Drawing.Point(18, 15);
+            this.question1.Margin = new System.Windows.Forms.Padding(4, 5, 6, 7);
+            this.question1.Name = "question1";
+            this.question1.Size = new System.Drawing.Size(414, 30);
+            this.question1.TabIndex = 1;
+            // 
+            // answer1
+            // 
+            this.answer1.BackColor = System.Drawing.SystemColors.Window;
+            this.answer1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.answer1.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.answer1.Location = new System.Drawing.Point(148, 211);
+            this.answer1.Name = "answer1";
+            this.answer1.Size = new System.Drawing.Size(149, 59);
+            this.answer1.TabIndex = 0;
+            this.answer1.Text = "Answer";
+            this.answer1.UseVisualStyleBackColor = false;
+            this.answer1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // random1
+            // 
+            this.random1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.random1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.random1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.random1.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.random1.Location = new System.Drawing.Point(106, 97);
+            this.random1.Name = "random1";
+            this.random1.Size = new System.Drawing.Size(236, 100);
+            this.random1.TabIndex = 2;
+            this.random1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(183, 279);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 37);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Switch to: ";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // numericYes
+            // 
+            this.numericYes.DecimalPlaces = 1;
+            this.numericYes.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.numericYes.Location = new System.Drawing.Point(221, 63);
+            this.numericYes.Name = "numericYes";
+            this.numericYes.Size = new System.Drawing.Size(120, 20);
+            this.numericYes.TabIndex = 4;
+            this.numericYes.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            65536});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(106, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Drop probability \"Yes\"";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(474, 348);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Random";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericYes)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button answer1;
+        private System.Windows.Forms.TextBox question1;
+        private System.Windows.Forms.Label random1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericYes;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+
+    public partial class Form1 : Form
+    {
+        bool yes_no = false;
+
+        Dictionary<int, string> answers = new Dictionary<int, string>
+        {
+            {0, "It is certain"},
+            {1, "It is decidedly so"},
+            {2, "Without a doubt "},
+            {3, "Yes — definitely"},
+            {4, "You may rely on it"},
+
+            {5, "As I see it, yes"},
+            {6, "Most likely"},
+            {7, "Outlook good"},
+            {8, "Signs point to yes"},
+            {9, "Yes"},
+
+            {10, "Reply hazy, try again"},
+            {11, "Ask again later"},
+            {12, "Better not tell you now"},
+            {13, "Cannot predict now"},
+            {14, "Concentrate and ask again"},
+
+            {15, "Don’t count on it"},
+            {16, "My reply is no"},
+            {17, "My sources say no"},
+            {18, "Outlook not so good"},
+            {19, "Very doubtful"}
+        };
+        Dictionary<int, double> menya_zastavil_nikita = new Dictionary<int, double>
+        {
+            {0,  0.1},
+            {1,  0.016},
+            {2,  0.016},
+            {3,  0.05},
+            {4,  0.016},
+
+            {5,  0.016},
+            {6,  0.1 },
+            {7,  0.016},
+            {8,  0.016},
+            {9,  0.1},
+
+            {10, 0.1 },
+            {11, 0.024},
+            {12, 0.1},
+            {13, 0.016},
+            {14, 0.016},
+
+            {15, 0.1 },
+            {16, 0.016},
+            {17, 0.15},
+            {18, 0.016},
+            {19, 0.016}
+        };
+
+        Dictionary<int, Color> colors = new Dictionary<int, Color>
+        {
+            {0,  Color.Blue},
+            {1,  Color.Blue},
+            {2,  Color.Blue},
+            {3,  Color.Blue},
+            {4,  Color.Blue},
+            {5,  Color.Green},
+            {6,  Color.Green},
+            {7,  Color.Green },
+            {8,  Color.Green },
+            {9,  Color.Green   },
+            {10, Color.Yellow },
+            {11, Color.Yellow },
+            {12, Color.Yellow },
+            {13, Color.Yellow },
+            {14, Color.Yellow },
+            {15, Color.Red },
+            {16, Color.Red  },
+            {17, Color.Red  },
+            {18, Color.Red },
+            {19, Color.Red }
+        };
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Text = "Switch to: Yes-No";
+            label1.Visible = numericYes.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            Console.WriteLine(yes_no);
+            if (question1.Text != "")
+            {
+                if (yes_no)
+                {
+                    Random rand = new Random();
+                    yes_no = true;
+                    var e_boi = rand.NextDouble() < (double)numericYes.Value;
+                    random1.Text = e_boi ? "Yes" : "No";
+                    random1.BackColor = e_boi ? Color.Green : Color.Red;
+                }
+                else
+                {
+                    Random rand = new Random();
+
+                    double A = rand.NextDouble();
+
+                        for (int k = 0; k <= 19; k++)
+                        {
+                            A -= menya_zastavil_nikita[k];
+                            if (A <= 0) { random1.Text = answers[k]; random1.BackColor = colors[k]; break; }
+                        };
+                };
+            }
+            else { random1.Text = "Where is the question??"; };
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (button1.Text == "Switch to: Yes-No")
+            {
+                yes_no = true;
+                random1.Text = "";
+                random1.BackColor = Color.White;
+                label1.Visible = numericYes.Visible = true;
+                button1.Text = "Switch to: Magic Ball";
+            }
+            else
+            {
+                label1.Visible = numericYes.Visible = false;
+                yes_no = false;
+                random1.BackColor = Color.White;
+                random1.Text = "";
+                button1.Text = "Switch to: Yes-No";
+            };
+        }
+    }
+}
